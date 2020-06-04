7c25904ada828daed1960b08b345b5f5f62fa040
Joseph Wagner <joseph.wagner@hpe.com>
2020-06-03 22:04:37 -0400

    Add chapter 12 traits notes

--- src/main/scala/chp12Traits/Traits.scala
+++ src/main/scala/chp12Traits/Traits.scala
@@ -23,6 +23,35 @@ class Rational(n: Int, d: Int) extends Ordered[Rational] {
   def compare(that: Rational): Int = (this.n * that.d) - (this.d * that.n)
 }
 
+abstract class IntQueue {
+  def get(): Int
+  def put(x: Int): Unit
+}
+
+class BasicIntQueue extends IntQueue {
+  import collection.mutable.ArrayBuffer
+  private val buf = new ArrayBuffer[Int]
+  def get() = buf.remove(0)
+  def put(x: Int) = { buf += x }
+}
+
+// This trait declares `IntQueue` as a superclass meaning that only classes
+// that also extend `IntQueue` can mix-in Doubling
+// Note: an abstract method calling `super` is normally illegal, but for traits this works
+// as long as the trait is mixed in after the method has been given a conrete implementation.
+// This is a common pattern for traits implement "stackable modifications"
+trait Doubling extends IntQueue {
+  abstract override def put(x: Int) = { super.put(2 * x) }
+}
+
+trait Incrementing extends IntQueue {
+  abstract override def put(x: Int) = { super.put(x + 1) }
+}
+
+trait Filtering extends IntQueue {
+  abstract override def put(x: Int) = { if (x >= 0) super.put(x) }
+}
+
 object Traits {
   val frog = new Frog()
   frog.philosophize()
@@ -32,6 +61,17 @@ object Traits {
   val phil: Philosophical = frog
   phil.philosophize()
 
+  class MyQueue extends BasicIntQueue with Doubling
+  val queue = new MyQueue
+  queue.put(10)
+  queue.get() // returns 20
+
+  // Mixing in traits during instantiation
+  // Mix order matters! Trait to the right are called first:
+  // Filtering's get() will exec before Incrementing's get(), but they BOTH get called. Hence "stacking"
+  val queue2 = new BasicIntQueue with Incrementing with Filtering
+  queue2.put(10); queue2.put(-1100)
+  queue2.get()
 
 }
 
